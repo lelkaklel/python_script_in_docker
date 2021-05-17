5f5e46e169efdc98e003d95c7df6aa0bcfe20624
Aleksey Soloviov <lelkaklel@gmail.com>
2021-05-17 19:46:57 +0300

    some changes

--- scripts/script.py
+++ scripts/script.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+import sys
 import time
 
 
 def main():
     while True:
-        print(f'Now is {time.strftime("%H:%M:%S", time.localtime())}')
+        print(f'Now is {time.strftime("%H:%M:%S", time.localtime())}', file=sys.stderr)
         time.sleep(1)
 
 
